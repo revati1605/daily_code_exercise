ed436815e84c5711cbe120421870d94fc25d6ab4
revati1605 <revatidharma16@gmail.com>
2022-06-01 16:54:43 +0530

    5 programs of 1/6

---
+++ june_01/program3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a=10;
+char b='a';
+float z=12.4;
+double  w=23.4;
+printf("size of a-%d\n size of b=%d\n size of z=%d\n size of w=%d\n",sizeof(a),sizeof(b),sizeof(z),sizeof(w));
+}
---
+++ june_01/program4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a value:\n");
+scanf("%d",&a);
+if(a%2==0)
+printf("%d is even number\n",a);
+else
+printf("%d is odd number\n",a);
+}
+
