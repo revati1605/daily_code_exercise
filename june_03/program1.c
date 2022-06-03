3e9f97c2ea028f439e2a7b19e879fde99ca47adb
revati1605 <revatidharma16@gmail.com>
2022-06-03 16:57:01 +0530

    3/6 programs

--- june_03/program1.c
+++ june_03/program1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-char s[20];
-int i;
-printf("enter string:\n");
-scanf("%s",s);
-for(i=0;s[i];i++);
-printf("length of string=%d\n",i);
+	char s[20];
+	int i;
+	printf("enter string:\n");
+	scanf("%[^\n]",s);
+	for(i=0;s[i];i++);
+	printf("length of string=%d\n",i);
 }
